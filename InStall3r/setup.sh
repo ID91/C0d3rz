ef57644d70dfe400a4584b4c4fb5356b2c271874
ID91 <39537464+ID91@users.noreply.github.com>
2018-05-22 23:13:13 +0000

    Add files via upload

---
+++ InStall3r/setup.sh
@@ -0,0 +1,12 @@
+apt-get upgrade && update
+apt-get install python2
+pkg install python
+apt-get install git
+pkg install curl
+pkg install php
+pkg install toilet
+pip2 install mechanize
+pkg install vim -y
+apt-get install ruby
+gem install bundle
+gem install lolcat
